93c28b45f28df820b59362b7e3bacad10b9ab2c9
successs404 <neoruien@u.nus.edu>
2021-04-01 11:06:43 +0800

    Refactor code

--- src/components/TopPanel.jsx
+++ src/components/TopPanel.jsx
@@ -1,65 +1,61 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import DatePicker from 'react-date-picker';
 import TimePicker from 'react-time-picker';
 import Slider from '@material-ui/core/Slider';
 
-function TopPanel() {
-    const [startDateValue, onStartDateChange] = useState(new Date());
-    const [startTimeValue, onStartTimeChange] = useState('10:00');
-    const [endDateValue, onEndDateChange] = useState(new Date());
-    const [endTimeValue, onEndTimeChange] = useState('10:00');
-    const [sizeValue, onSizeChange] = useState(7);
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <span>Start: </span>
-                    <DatePicker
-                        onChange={onStartDateChange}
-                        value={startDateValue}
-                        minDate={new Date('2013-10-02')}
-                    />
-                    <TimePicker
-                        onChange={onStartTimeChange}
-                        value={startTimeValue}
-                    />
-                </Col>
-                <Col>
-                    <span>End: </span>
-                    <DatePicker
-                        onChange={onEndDateChange}
-                        value={endDateValue}
-                        maxDate={new Date('2013-12-03')}
-                    />
-                    <TimePicker
-                        onChange={onEndTimeChange}
-                        value={endTimeValue}
-                    />
-                </Col>
-                <Col>
-                    <Slider
-                        defaultValue={80}
-                        getAriaValueText={valuetext}
-                        aria-labelledby="discrete-slider-always"
-                        step={10}
-                        marks={sizeValue}
-                        valueLabelDisplay="on"
-                    />
-                    {/* <IonRangeSlider type={"single"} min={1} max={10} step={1} /> */}
-                    {/* <Form>
-                        <Form.Group controlId="formBasicRange">
-                            <Form.Label>Range</Form.Label>
-                            <Form.Control type="range" value={sizeValue} onChange={onSizeChange}/>
-                        </Form.Group>
-                    </Form> */}
-                    <span>2^{sizeValue} = 128 samples</span>
-                </Col>
-            </Row>
-        </Container>
-    );
+class TopPanel extends Component {
+    debug = () => {
+        console.log("DEBUG")
+        console.log("DATE: " + this.props.startDate)
+    }
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <span>Start: </span>
+                        <DatePicker
+                            onChange={this.props.onStartDateChange}
+                            selected={this.props.startDate}
+                        />
+                        <TimePicker
+                            onChange={this.props.onStartTimeChange}
+                            value={this.props.startTime}
+                        />
+                    </Col>
+                    <Col>
+                        <span>End: </span>
+                        <DatePicker
+                            onChange={this.props.onEndDateChange}
+                            value={this.props.endDate}
+                        />
+                        <TimePicker
+                            onChange={this.props.onEndTimeChange}
+                            value={this.props.endTime}
+                        />
+                    </Col>
+                    <Col>
+                        <Slider
+                            step={1}
+                            min={1}
+                            max={10}
+                            value={this.props.size}
+                            onChange={this.props.onSizeChange}
+                            valueLabelDisplay="on"
+                        />
+                        <span>2^{this.props.size} = {Math.pow(2, this.props.size)} samples</span>
+                    </Col>
+                </Row>
+                <Row>
+                    <Button onClick={this.debug}>DEBUG</Button>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default TopPanel
